3e5f33786245b3076b56ab5eb2ded20f2f109006
wisehead <alex.chenhui@gmail.com>
2019-07-16 10:57:25 +0800

    rport

---
+++ rport/sys_time.h
@@ -0,0 +1,19 @@
+//  Copyright (c) 2011-present, Facebook, Inc.  All rights reserved.
+//  This source code is licensed under the BSD-style license found in the
+//  LICENSE file in the root directory of this source tree. An additional grant
+//  of patent rights can be found in the PATENTS file in the same directory.
+//
+// Copyright (c) 2011 The LevelDB Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file. See the AUTHORS file for names of contributors.
+
+// This file is a portable substitute for sys/time.h which does not exist on
+// Windows
+
+#ifndef STORAGE_LEVELDB_PORT_SYS_TIME_H_
+#define STORAGE_LEVELDB_PORT_SYS_TIME_H_
+
+#include <time.h>
+#include <sys/time.h>
+
+#endif  // STORAGE_LEVELDB_PORT_SYS_TIME_H_
